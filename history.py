8468d0dd4498064fc94f073f26fa21f5441de544
Corneille Ngoy <corneillengoy@gmail.com>
2025-08-25 19:35:24 -0700

    Initial commit of Spyder project

---
+++ history.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Sat Feb  1 23:12:06 2025)---
+%runfile C:/Users/corne/.spyder-py3/temp.py --wdir
+%runfile C:/Users/corne/OneDrive/Desktop/electric/electric.py --wdir
+
+## ---(Sun Feb  2 18:10:43 2025)---
+%runfile C:/Users/corne/OneDrive/Desktop/electric/electric.py --wdir
+%runfile C:/Users/corne/OneDrive/Desktop/electric/electric.py --args C:/Users/corne/OneDrive/Desktop/electric/input_1.txt --wdir
+%runfile C:/Users/corne/OneDrive/Desktop/electric/electric.py --args C:/Users/corne/OneDrive/Desktop/electric/input_2.txt --wdir
+%runfile C:/Users/corne/OneDrive/Desktop/electric/electric.py --args C:/Users/corne/OneDrive/Desktop/electric/input_1.txt --wdir
+
+## ---(Sat Aug  2 14:47:01 2025)---
+%runfile C:/Users/corne/OneDrive/Desktop/electric/untitled0.py --wdir
+12
+%runfile C:/Users/corne/OneDrive/Desktop/electric/untitled0.py --wdir
+
+## ---(Thu Aug 14 18:16:49 2025)---
+%runfile C:/Users/corne/.spyder-py3/temp.py --wdir
+
+## ---(Thu Aug 14 18:50:35 2025)---
+%runfile C:/Users/corne/.spyder-py3/temp.py --wdir
+
+## ---(Fri Aug 15 17:27:04 2025)---
+%runfile C:/Users/corne/.spyder-py3/temp.py --wdir
+
+## ---(Sat Aug 16 15:24:58 2025)---
+%runfile C:/Users/corne/.spyder-py3/temp.py --wdir
+
+## ---(Thu Aug 21 16:15:40 2025)---
+%runfile C:/Users/corne/Downloads/aplusb/aplusb/python3.py --wdir
+bash run-tests.sh python3
+
+## ---(Mon Aug 25 18:28:56 2025)---
+%runfile C:/Users/corne/.spyder-py3/temp.py --wdir
